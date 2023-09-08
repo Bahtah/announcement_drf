a2eaf25c6e942e783d98e5383cc20e5cff315bc1
Bahtah <btolikov@gmail.com>
2023-09-08 20:15:44 +0300

    add swagger

--- src/settings.py
+++ src/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'drf_yasg',
     
     'users.apps.UsersConfig',
     'announcement.apps.AnnouncementConfig',
